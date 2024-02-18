9b8cab0d989c9086393d6d47db057ab531edc7e6
avrubio <ariadna.vanegas@gmail.com>
2024-02-18 01:09:32 -0500

    created sections 2 and 3, section 3 wip

---
+++ tailwingConfig.js
@@ -0,0 +1,19 @@
+module.exports = {
+  //...
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          first: "#86A8FF",
+          secondary: "#F5070F",
+          accent: "#F60CD7",
+          neutral: "#F8A348",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+};
